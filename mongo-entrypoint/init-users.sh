c4c2f14dd677db3c258d67524447998561f958e7
zennomi <zennomi.kun@gmail.com>
2025-03-07 13:36:00 +0700

    Refactor MongoDB configuration and initialization scripts

--- mongo-entrypoint/init-users.sh
+++ mongo-entrypoint/init-users.sh
@@ -1,13 +1,13 @@
-if [ "$MONGO_INITDB_ROOT_USERNAME" ] && [ "$MONGO_INITDB_ROOT_PASSWORD" ]; then
-  "${mongo[@]}" "$MONGO_INITDB_DATABASE" <<-EOJS
+if [ "$MONGO_USERNAME" ] && [ "$MONGO_PASSWORD" ]; then
+  "${mongo[@]}" "$MONGO_DATABASE" <<-EOJS
   db.createUser({
-     user: $(_js_escape "$MONGO_INITDB_ROOT_USERNAME"),
-     pwd: $(_js_escape "$MONGO_INITDB_ROOT_PASSWORD"),
+     user: $(_js_escape "$MONGO_USERNAME"),
+     pwd: $(_js_escape "$MONGO_PASSWORD"),
      roles: [ "readWrite", "dbAdmin" ]
      })
 EOJS
 fi
 
 echo ======================================================
-echo created $MONGO_INITDB_ROOT_USERNAME in database $MONGO_INITDB_DATABASE
+echo created $MONGO_USERNAME in database $MONGO_DATABASE
 echo ======================================================
