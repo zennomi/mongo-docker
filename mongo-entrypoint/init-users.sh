087dc4031aa847d5a0596cd2942ef45e551e583f
zennomi <zennomi.kun@gmail.com>
2025-03-07 14:17:54 +0700

    Remove MONGO_DATABASE from environment variables and update user creation script to use admin database with root privileges

--- mongo-entrypoint/init-users.sh
+++ mongo-entrypoint/init-users.sh
@@ -1,13 +1,13 @@
 if [ "$MONGO_USERNAME" ] && [ "$MONGO_PASSWORD" ]; then
-  "${mongo[@]}" "$MONGO_DATABASE" <<-EOJS
+  "${mongo[@]}" "admin" <<-EOJS
   db.createUser({
      user: $(_js_escape "$MONGO_USERNAME"),
      pwd: $(_js_escape "$MONGO_PASSWORD"),
-     roles: [ "readWrite", "dbAdmin" ]
+     roles: [ "root" ]
      })
 EOJS
 fi
 
 echo ======================================================
-echo created $MONGO_USERNAME in database $MONGO_DATABASE
+echo Created $MONGO_USERNAME in the admin database with root privileges
 echo ======================================================
